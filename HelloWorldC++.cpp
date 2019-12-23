f0c394b92d5cf5867037f174dab3deb2550ec484
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2019-12-23 22:00:07 +0100

    Upload code for HelloWorldCpp project.

---
+++ HelloWorldC++.cpp
@@ -0,0 +1,12 @@
+// HelloWorldC++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World C++!\n\r";
+	return 0;
+}
+
